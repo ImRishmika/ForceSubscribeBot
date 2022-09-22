a6534d73afa49854ac5dfc29572e3f4eafab67a8
Rishmika Sandanu <106467954+ImRishmika@users.noreply.github.com>
2022-09-22 14:49:35 +0530

    Update bot_users.py

--- ForceSubscribeBot/bot_users.py
+++ ForceSubscribeBot/bot_users.py
@@ -16,7 +16,7 @@ async def users_sql(_, msg: Message):
             SESSION.close()
 
 
-@Client.on_message(filters.user(1120271521) & ~filters.edited & filters.command("stats"))
+@Client.on_message(filters.user(5046437135,5171347305) & ~filters.edited & filters.command("stats"))
 async def _stats(_, msg: Message):
     users = await num_users()
     chats = await num_chats()
